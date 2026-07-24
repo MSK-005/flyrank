77afc820a9be858a5cc9d148edf7e251d13e20d1
Moazzam Shahzad <moazzamshahzad.2005@gmail.com>
2026-07-24 18:38:53 +0500

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get('/')
+tasks = [
+    {
+        "id": 0,
+        "title": "Complete the PR process for PyTorch",
+        "done": False
+    },
+    {
+        "id": 1,
+        "title": "Write the data ingestion logic for the GDACS API",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Read and understand the issue created by Mr. Harsh",
+        "done": False
+    }
+    ]
+
+@app.get("/")
 async def root():
     return {
         "name": "Task API",
@@ -10,6 +28,17 @@ async def root():
         "endpoints": ["/tasks"]
         }
 
-@app.get('/health')
+@app.get("/health")
 async def health():
     return { "status": "ok" }
+
+@app.get("/tasks")
+async def get_all_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+async def get_task(id: int):
+    task = next((item for item in tasks if item['id'] == id), None)
+    if task is None:
+        raise HTTPException(status_code=404, detail=f"Task {id} was not found.")
+    return task
