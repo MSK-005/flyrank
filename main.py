65fb80011e42f96f0ae61af054917c7122fe7ceb
Moazzam Shahzad <moazzamshahzad.2005@gmail.com>
2026-07-26 20:02:35 +0500

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
+import itertools
 
 app = FastAPI()
+_id_iter:int = itertools.count()
+
+class Task(BaseModel):
+
+    id:int = Field(default_factory=lambda: next(_id_iter))
+    title:str
+    done:bool = False
 
 tasks = [
-    {
-        "id": 0,
-        "title": "Complete the PR process for PyTorch",
-        "done": False
-    },
-    {
-        "id": 1,
-        "title": "Write the data ingestion logic for the GDACS API",
-        "done": False
-    },
-    {
-        "id": 2,
-        "title": "Read and understand the issue created by Mr. Harsh",
-        "done": False
-    }
-    ]
+    Task(title="Complete the PR process for PyTorch"),
+    Task(title="Write the data ingestion logic for the GDACS API"),
+    Task(title="Read and understand the issue created by Mr. Harsh")
+]
 
 @app.get("/")
 async def root():
@@ -38,7 +35,17 @@ async def get_all_tasks():
 
 @app.get("/tasks/{id}")
 async def get_task(id: int):
-    task = next((item for item in tasks if item['id'] == id), None)
+    task = next((item for item in tasks if item.id == id), None)
     if task is None:
         raise HTTPException(status_code=404, detail=f"Task {id} was not found.")
+    return task
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+async def create_task(res:dict):
+    title = res.get('title')
+    if title is None or str(title).strip() == '':
+        raise HTTPException(status_code=400, detail="Missing res")
+    title = str(title).strip()
+    task = Task(title=title)
+    tasks.append(task)
     return task
